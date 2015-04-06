b4c7a057c6fd7c23f633e1eb77e0d3b8d532746b
Reid Baldwin & Jill Kuchman <student@epicodus.com>
2015-04-06 16:12:17 -0700

    Add starting consonant method and test

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,39 @@
 var pigLatin = function(input) {
     var split_input = input.split('');
     var vowels = ["a", "e", "i", "o", "u"];
-    if (($.inArray(split_input[0], vowels)) !== -1) {
-        return input+"ay";
-    }
-    else {
-        return false;
-    }
+    var output;
+    var completed_word = false;
+
+    split_input.forEach(function(letter) {
+        vowels.forEach(function(vowel) {
+            if (!completed_word) {
+                if(letter === vowel) {
+                    var letter_index = split_input.indexOf(letter);
+                    var consonant_array = split_input.slice(0,letter_index);
+                    var input_length = input.length;
+                    var vowel_array = split_input.slice(letter_index, input_length);
+                    output = vowel_array.join('') + consonant_array.join('') + "ay";
+                    console.log(output);
+                    completed_word = true;
+                    return output;
+                }
+            }
+        });
+    });
+
+    return output;
 };
+
+
+// we will want to split the array at the first vowel
+//foreach loop?
+    //foreach split_input as letter
+    //foreach vowels as vowel
+    //if letter === vowel, then split the array into two strings at that point
+//split into 2 strings: beginning consonants and rest of word starting with the first vowel
+//return vowel_string+consonant_string+"ay"
+
+//if the first argument is not found in the second argument, -1 is returned, so !== -1 evaluates true
+// if (($.inArray(split_input[0], vowels)) !== -1) {
+//     return input+"ay";
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,8 @@
 describe('pigLatin', function() {
-    it("is false for a word that does not end in -ay", function() {
-        expect(pigLatin("fox")).to.equal(false);
-    });
     it("returns the same word with -ay at the end if the word begins with a vowel", function() {
         expect(pigLatin("apple")).to.equal("appleay");
+    });
+    it("returns the word with the vowel sound at the beginning, followed by the starting consonants followed by -ay", function() {
+        expect(pigLatin("fox")).to.equal("oxfay");
     });
 });
